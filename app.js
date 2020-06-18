3ae1e033ba6b60147f9bc0e92c4809c771f3891f
cihankarsak <cihankarsak@gmail.com>
2020-06-18 16:44:05 +0300

    mongoose eklendi

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 var http = require('http');
+//Mongoose 'ı ekledik
+var mongoose = require('mongoose');
+//User Schema mız
+var User = require('./user');
+
+const uri = 'mongodb+srv://testUser:Test1234@cihankarsak.exgxd.mongodb.net/testDB';
+
+mongoose.connect(uri, {useNewUrlParser: true }).then( () => {
+  console.log('MongoDB ye bağlanti başarili')
+})
+.catch( (err) => console.error(err));
+
 
 /**
  * Dinleyeceğimiz port numarasını aws içindeki environment larda PORT değişkeninden alalım eğer Port değişkeni yok ise default olarak 8081 portunu i dinleyelim.
@@ -7,6 +19,11 @@ var port = normalizePort(process.env.PORT || '8081');
 
 const express = require('express');
 const app = express();
+
+/* BodyPaser*/
+var bodyparser = require('body-parser');
+var urlencoded = bodyparser.urlencoded({extended: false});
+
 
 /**
  * bir get apisi ile ekrana mesajımızı yazdıralım
@@ -41,3 +58,57 @@ function normalizePort(val) {
     return false;
   }
 
+
+/* CRUD Fonksiyon Örneklerimiz */
+app.post('/getUserByName',urlencoded,(req,res) => {
+  User.find({name:req.body.name},(err,user) => {
+    if (err){  
+      throw err;
+    }
+    res.send(user);
+  })
+})
+
+
+app.post('/getUserById',urlencoded,(req,res) => {
+  User.findById(req.body.id,(err,user) => {
+    if (err){  
+      throw err;
+    }
+    res.send(user);
+  })
+})
+
+app.post('/getUserByWhereEmail',urlencoded,(req,res) => {
+  User.find({},(err,user) => {
+    if (err){  
+      throw err;
+    }
+    res.send(user);
+  }).where('email').equals(req.body.email);
+})
+
+app.post('/getUserByWhereEmailLimit',urlencoded,(req,res) => {
+  User.find({},(err,user) => {
+    if (err){  
+      throw err;
+    }
+    res.send(user);
+  }).where('email').equals(req.body.email).limit(1);
+})
+
+/**
+ * bir post apisi ile ekrana mesajımızı yazdıralım
+ */
+app.post('/create_user',urlencoded,(req,res) => {
+  var user = new User({
+    name: req.body.name,
+    email:req.body.email
+  });
+  
+  user.save().then(() => {
+    console.log('kayit başarili');
+    res.send(req.body.name + " isimli User kayit edildi");
+  })
+  .catch( (err) => res.send(error + " hatası alındı") );
+})
